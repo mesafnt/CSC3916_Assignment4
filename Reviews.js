adababd84419f373fa1b96a8e83a939d7fa11756
mesafnt <mesafnt.kalu@ucdenver.edu>
2023-04-02 13:02:30 -0600

    update 2

--- Reviews.js
+++ Reviews.js
@@ -18,7 +18,7 @@ var ReviewSchema = new Schema({
     user_id: {type: Schema.Types.ObjectID, ref: "UserSchema", required: true},
     movie_id: { type: Schema.Types.ObjectId, ref: "MovieSchema", required: true},
     username: { type: String, required: true},
-    small_quote: { type: String, required: true},
+    review: { type: String, required: true},
     rating: { type: Number, min: 1, max: 5, required: true}
 });
 
