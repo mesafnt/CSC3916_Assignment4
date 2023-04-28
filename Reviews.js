5433a1af196ddc274507b6f58e10d226d2d57642
mesafnt <mesafnt.kalu@ucdenver.edu>
2023-04-27 19:22:50 -0600

    update 1

--- Reviews.js
+++ Reviews.js
@@ -16,7 +16,7 @@ mongoose.set('useCreateIndex', true);
 //reviews schema
 var ReviewSchema = new Schema({
     user_id: {type: Schema.Types.ObjectID, ref: "UserSchema", required: true},
-    movie_id: { type: Schema.Types.ObjectId, ref: "MovieSchema", required: true},
+    movie_id: { type: mongoose.Types.ObjectId, required: true},
     username: { type: String, required: true},
     review: { type: String, required: true},
     rating: { type: Number, min: 1, max: 5, required: true}
